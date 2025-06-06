6a06ec2a808fd4fc36071bc55af321bc37a3a7be
Trong Hieu <anhtheboy022@gmail.com>
2025-06-06 15:38:53 +0700

    fix config vercel

--- Server/config/MongoDb.js
+++ Server/config/MongoDb.js
@@ -1,14 +1,39 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
+// const connectDatabase = async () => {
+//   // mongodb+srv://admin:admin@foodies.hplmw.mongodb.net/?retryWrites=true&w=majority&appName=foodies
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URL, {});
+//     console.log(`MongoDB Connected`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+let isConnected = false;
+
 const connectDatabase = async () => {
-  // mongodb+srv://admin:admin@foodies.hplmw.mongodb.net/?retryWrites=true&w=majority&appName=foodies
+  if (isConnected) {
+    return;
+  }
+
+  if (mongoose.connections[0].readyState) {
+    isConnected = true;
+    return;
+  }
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {});
-    console.log(`MongoDB Connected`);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('MongoDB Connected');
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error('MongoDB Connection Error:', error);
+    throw new Error('MongoDB connection failed');
   }
 };
 
